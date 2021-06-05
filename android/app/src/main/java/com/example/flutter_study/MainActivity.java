13f900c4c615cc95394071a3a8426c411fa88646
lucywang <351328420@qq.com>
2021-06-05 10:29:41 +0800

    [fix]配置

--- android/app/src/main/java/com/example/flutter_study/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_study/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.flutter_study;
 
 import io.flutter.embedding.android.FlutterActivity;
+// import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+  // @Override
+  // protected void onCreate(Bundle savedInstanceState) {
+  //   super.onCreate(savedInstanceState);
+  //   if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+  //     getWindow().setStatusBarColor(0);
+  //   }
+  //   GeneratedPluginRegistrant.registerWith(this);
+  // }
 }
